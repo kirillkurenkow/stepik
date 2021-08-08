83b5023265aca08f0cda8cbc974b6580a63ef1d9
Kirill Kurenkov <kkurenkov@parallels.com>
2021-08-08 18:15:30 +0300

    Complete course_86343.module_4.part_4.step_10

---
+++ course_86343/module_4/part_4/step_10.py
@@ -0,0 +1,32 @@
+"""
+След матрицы
+
+Следом квадратной матрицы называется сумма элементов главной диагонали.
+Напишите программу, которая выводит след заданной квадратной матрицы.
+
+Формат входных данных
+На вход программе подаётся натуральное число n — количество строк и столбцов в матрице,
+затем элементы матрицы (целые числа) построчно через пробел.
+
+Формат выходных данных
+Программа должна вывести одно число — след заданной матрицы.
+"""
+import sys
+import os
+
+DEBUG = True
+if DEBUG:
+    input_filename = 'input.txt'
+    if os.path.exists(input_filename):
+        sys.stdin = open(input_filename, encoding='utf-8')
+
+
+def main():
+    number = int(input())
+    matrix = [[int(x) for x in input().split()] for _ in range(number)]
+    result = sum((matrix[x][x] for x in range(number)))
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
