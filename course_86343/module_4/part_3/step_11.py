4b132e2fc7c08c76b59af4d7017b2eefe5741dfb
Kirill Kurenkov <kkurenkov@parallels.com>
2021-08-08 17:16:10 +0300

    Complete course_86343.module_4.part_3.step_11

---
+++ course_86343/module_4/part_3/step_11.py
@@ -0,0 +1,37 @@
+"""
+Треугольник Паскаля 2
+На вход программе подается натуральное число n. Напишите программу, которая выводит первые n строк треугольника Паскаля.
+
+Формат входных данных
+На вход программе подается число n (n ≥ 1).
+
+Формат выходных данных
+Программа должна вывести первые nn строк треугольника Паскаля, каждую на отдельной строке в соответствии с образцом.
+"""
+import sys
+import os
+
+DEBUG = True
+if DEBUG:
+    input_filename = 'input.txt'
+    if os.path.exists(input_filename):
+        sys.stdin = open(input_filename, encoding='utf-8')
+
+
+def main():
+    result = [
+        [1],
+        [1, 1],
+    ]
+    number = int(input())
+    if number < 2:
+        [print(*row) for row in result[:number]]
+    else:
+        for i in range(1, number - 1):
+            new_row = [1] + [result[i][j] + result[i][j + 1] for j in range(len(result[i]) - 1)] + [1]
+            result.append(new_row)
+        [print(*row) for row in result]
+
+
+if __name__ == '__main__':
+    main()
