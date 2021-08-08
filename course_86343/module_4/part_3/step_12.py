4566da066ad8751a3db8ee008054854a9673177e
Kirill Kurenkov <kkurenkov@parallels.com>
2021-08-08 17:19:14 +0300

    Complete course_86343.module_4.part_3.step_12

---
+++ course_86343/module_4/part_3/step_12.py
@@ -0,0 +1,34 @@
+"""
+Упаковка дубликатов
+На вход программе подается строка текста, содержащая символы. Напишите программу,
+которая упаковывает последовательности одинаковых символов заданной строки в подсписки.
+
+Формат входных данных
+На вход программе подается строка текста, содержащая символы, отделенные символом пробела.
+
+Формат выходных данных
+Программа должна вывести указанный вложенный список.
+"""
+import sys
+import os
+
+DEBUG = True
+if DEBUG:
+    input_filename = 'input.txt'
+    if os.path.exists(input_filename):
+        sys.stdin = open(input_filename, encoding='utf-8')
+
+
+def main():
+    string = input().split()
+    result = list()
+    for char in string:
+        if result and char in result[-1]:
+            result[-1].append(char)
+        else:
+            result.append([char])
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
