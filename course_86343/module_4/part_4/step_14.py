2ef032c232fc5be41b5152d00d54c63766865bd2
Kirill Kurenkov <kkurenkov@parallels.com>
2021-08-08 18:43:45 +0300

    Complete course_86343.module_4.part_4.step_14

---
+++ course_86343/module_4/part_4/step_14.py
@@ -0,0 +1,36 @@
+"""
+
+"""
+import sys
+import os
+
+DEBUG = True
+if DEBUG:
+    input_filename = 'input.txt'
+    if os.path.exists(input_filename):
+        sys.stdin = open(input_filename, encoding='utf-8')
+
+
+def main():
+    number = int(input())
+    matrix = [list(map(int, input().split())) for _ in range(number)]
+    upper, lower, left, right = list(), list(), list(), list()
+    for i in range(number):
+        for j in range(number):
+            element = matrix[i][j]
+            if j > i < number - 1 - j:
+                upper.append(element)
+            elif j < i > number - 1 - j:
+                lower.append(element)
+            elif j < i < number - 1 - j:
+                left.append(element)
+            elif j > i > number - 1 - j:
+                right.append(element)
+    print(f'Верхняя четверть: {sum(upper)}')
+    print(f'Правая четверть: {sum(right)}')
+    print(f'Нижняя четверть: {sum(lower)}')
+    print(f'Левая четверть: {sum(left)}')
+
+
+if __name__ == '__main__':
+    main()
