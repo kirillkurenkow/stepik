9f5363731a5774de08481dfe7e1d36ade96eb4a2
Kirill Kurenkov <kkurenkov@parallels.com>
2021-08-08 20:45:33 +0300

    Complete course_86343.module_4.part_6.step_6

---
+++ course_86343/module_4/part_6/step_6.py
@@ -0,0 +1,37 @@
+"""
+Заполнение 4
+
+На вход программе подается натуральное число n.
+Напишите программу, которая создает матрицу размером n × n заполнив её в соответствии с образцом.
+
+Формат входных данных
+На вход программе подается натуральное число n — количество строк и столбцов в матрице.
+
+Формат выходных данных
+Программа должна вывести указанную матрицу в соответствии с образцом.
+
+Примечание. Для вывода элементов матрицы как в примерах, отводите ровно 3 символа на каждый элемент.
+Для этого используйте строковый метод ljust(). Можно обойтись и без ljust(), система примет и такое решение.
+"""
+import sys
+import os
+
+DEBUG = True
+if DEBUG:
+    input_filename = 'input.txt'
+    if os.path.exists(input_filename):
+        sys.stdin = open(input_filename, encoding='utf-8')
+
+
+def main():
+    number = int(input())
+    matrix = [[0 for _ in range(number)] for _ in range(number)]
+    for i in range(number):
+        for j in range(number):
+            if j >= i <= number - j - 1 or j <= i >= number - j - 1:
+                matrix[i][j] = 1
+    [print(*row) for row in matrix]
+
+
+if __name__ == '__main__':
+    main()
