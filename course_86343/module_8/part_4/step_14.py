00ed92357f8050619b5c5655157f2272232825f7
Kirill Kurenkov <kirillkurenkow@gmail.com>
2021-08-17 01:45:17 +0300

    Complete course_86343.module_8.part_4.step_14

---
+++ course_86343/module_8/part_4/step_14.py
@@ -0,0 +1,31 @@
+"""
+Все 10 цифр
+
+На вход программе подаются две строки, состоящие из цифр.
+Необходимо определить, верно ли, что в записи этих двух строк используются все десять цифр?
+
+Формат входных данных
+На вход подаются две строки, состоящие из цифр.
+
+Формат выходных данных
+Программа должна вывести YES, если в записи этих двух строк используются все десять цифр, и NO в противном случае.
+"""
+import sys
+
+DEBUG = True
+
+
+def set_debug_input():
+    input_filename = 'input.txt'
+    sys.stdin = open(input_filename, encoding='utf-8')
+
+
+def main():
+    input_1, input_2 = input(), input()
+    print('YES' if set(input_1 + input_2) == set(map(str, range(10))) else 'NO')
+
+
+if __name__ == '__main__':
+    if DEBUG:
+        set_debug_input()
+    main()
