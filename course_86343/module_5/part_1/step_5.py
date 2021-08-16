408969439174354a25f83a9d5591f5554d65e1f1
Kirill Kurenkov <kkurenkov@parallels.com>
2021-08-16 22:19:26 +0300

    Complete course_86343.module_5.part_1.step_5

---
+++ course_86343/module_5/part_1/step_5.py
@@ -0,0 +1,36 @@
+"""
+Симметричная матрица
+
+Напишите программу проверки симметричности квадратной матрицы относительно побочной диагонали.
+
+Формат входных данных
+На вход программе подаётся натуральное число n — количество строк и столбцов в матрице, затем элементы матрицы.
+
+Формат выходных данных
+Программа должна вывести YES, если матрица симметрична, и слово NO в противном случае.
+"""
+import sys
+
+DEBUG = True
+
+
+def set_debug_input():
+    input_filename = 'input.txt'
+    sys.stdin = open(input_filename, encoding='utf-8')
+
+
+def main():
+    number = int(input())
+    matrix = [list(map(int, input().split())) for _ in range(number)]
+    for i in range(number):
+        for j in range(number):
+            if matrix[i][j] != matrix[number - j - 1][number - i - 1]:
+                print('NO')
+                return
+    print('YES')
+
+
+if __name__ == '__main__':
+    if DEBUG:
+        set_debug_input()
+    main()
