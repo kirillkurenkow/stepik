c5028ebbe04dd71242c99d2a35bf99e9b49accf7
Kirill Kurenkov <kirillkurenkow@gmail.com>
2021-08-17 01:42:50 +0300

    Complete course_86343.module_8.part_4.step_13

---
+++ course_86343/module_8/part_4/step_13.py
@@ -0,0 +1,31 @@
+"""
+Неповторимые цифры
+
+На вход программе подается строка, состоящая из цифр.
+Необходимо определить, верно ли, что в ее записи ни одна из цифр не повторяется?
+
+Формат входных данных
+На вход программе подается строка, состоящая из цифр
+
+Формат выходных данных
+Программа должна вывести YES, если ни одна из цифр в строке не повторяется и NO в противном случае.
+"""
+import sys
+
+DEBUG = True
+
+
+def set_debug_input():
+    input_filename = 'input.txt'
+    sys.stdin = open(input_filename, encoding='utf-8')
+
+
+def main():
+    input_ = input()
+    print('YES' if len(set(input_)) == len(input_) else 'NO')
+
+
+if __name__ == '__main__':
+    if DEBUG:
+        set_debug_input()
+    main()
