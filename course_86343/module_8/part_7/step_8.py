8adf4c242950f9e5fae1c15a8975ca97d9b92872
Kirill Kurenkov <kkurenkov@parallels.com>
2021-08-17 12:35:38 +0300

    Complete course_86343.module_8.part_7.step_8

---
+++ course_86343/module_8/part_7/step_8.py
@@ -0,0 +1,30 @@
+"""
+Одинаковые цифры
+
+На вход программе подаются два числа. Напишите программу, определяющую, есть ли в данных числах одинаковые цифры.
+
+Формат входных данных
+На вход программе подаются два натуральных числа, каждое на отдельной строке.
+
+Формат выходных данных
+Программа должна вывести YES, если в записи данных чисел есть одинаковые цифры и NO если нет.
+"""
+import sys
+
+DEBUG = True
+
+
+def set_debug_input():
+    input_filename = 'input.txt'
+    sys.stdin = open(input_filename, encoding='utf-8')
+
+
+def main():
+    set_1, set_2 = set(input()), set(input())
+    print('NO' if set_1.isdisjoint(set_2) else 'YES')
+
+
+if __name__ == '__main__':
+    if DEBUG:
+        set_debug_input()
+    main()
